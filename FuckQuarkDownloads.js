f7baaffe8f1e5c4efacb00002d30041c707209cb
soft98-top <soft1998@qq.com>
2023-02-09 10:35:58 +0800

    add support for dir

--- FuckQuarkDownloads.js
+++ FuckQuarkDownloads.js
@@ -13,22 +13,25 @@ try {
     document.children[0].appendChild(script);
     console.log('jQuery Loading!');
 }
-async function send(method,url,data="") {
+async function send(method,url,data="",jsonSwitch=true) {
     let ajaxConfig = {
         "type":method,
         "url":url,
-        "data":JSON.stringify(data),
-        "dataType":"json",
-        "contentType":"application/json;charset=UTF-8",
-        "headers":{
-            "Content-Type":`application/json;charset=UTF-8`
-        },
+        "data":data,
         "xhrFields":{
             "withCredentials":true
         }
     }
+    if (jsonSwitch) {
+        ajaxConfig["data"] = JSON.stringify(data);
+        ajaxConfig["dataType"] = "json";
+        ajaxConfig["contentType"] = "application/json;charset=UTF-8";
+        ajaxConfig["headers"] = {
+            "Content-Type":`application/json;charset=UTF-8`
+        };
+    }
     if(data==""){
-        ajaxConfig["data"] = null
+        ajaxConfig["data"] = null;
     }
     let retval = await $.ajax(ajaxConfig);
     return retval;
@@ -38,14 +41,47 @@ function fuckDownload(fid) {
     let data = {
         "fids":[]
     }
-    data.fids.push(fid);
+    if (typeof(fid) == "object") {
+        data.fids = fid;
+    }else{
+        data.fids.push(fid);
+    }
     send("POST","https://drive.quark.cn/1/clouddrive/file/download?pr=ucpro&fr=pc",data).then((data) => {
         if(data.code == 0){
-            window.open(data.data[0]["download_url"]);
+            for (let download_data of data.data) {
+                window.open(download_data["download_url"]);
+            }
         }else{
             alert("FuckDownload Fail, error: " + data["message"]);
         }
     })
+}
+
+function exportDir(task_data) {
+    send("GET","https://drive.quark.cn/1/clouddrive/download/list/export?pr=ucpro&fr=pc",task_data,false).then((data1) => {
+        if (data1 == "") {
+            setTimeout(exportDir(task_data),500);
+        }else{
+            let fid = data1.substring(data1.indexOf(':') + 1,data1.indexOf(';'));
+            fuckDownload(fid.split(","));
+        }
+    });
+}
+
+function fuckDownloadDir(fid) {
+    let data = {
+        "current_dir_fid":fid
+    };
+    send("POST","https://drive.quark.cn/1/clouddrive/download/list?pr=ucpro&fr=pc",data).then((data) => {
+        if(data.code == 0){
+            let task_data = {
+                "task_id":data.data['task_id']
+            };
+            exportDir(task_data);
+        }else{
+            alert("FuckDownload Fail, error: " + data["message"]);
+        }
+    });
 }
 
 function fuckDownloads() {
@@ -55,7 +91,11 @@ function fuckDownloads() {
         let fid = file.getAttribute("data-row-key");
         let sizeTd = file.getElementsByTagName("td")[2];
         if(sizeTd.getAttribute("isfucked") != "yes"){
-            sizeTd.innerHTML = sizeTd.innerText + "<button onclick='fuckDownload(\"" + fid + "\")'>-> Download <-</button>"
+            if (sizeTd.innerText == "-") {
+                sizeTd.innerHTML = sizeTd.innerText + "<button onclick='fuckDownloadDir(\"" + fid + "\")'>-> Download <-</button>";
+            }else{
+                sizeTd.innerHTML = sizeTd.innerText + "<button onclick='fuckDownload(\"" + fid + "\")'>-> Download <-</button>";
+            }
             sizeTd.setAttribute("isfucked","yes");
         }
     }
