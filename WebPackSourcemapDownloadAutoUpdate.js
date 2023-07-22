b2f046d53fdd5fd957e07e1fcca0f77fe1ab3bb1
soft98-top <soft1998@qq.com>
2023-07-22 20:19:36 +0800

    update reverse proxy

---
+++ WebPackSourcemapDownloadAutoUpdate.js
@@ -0,0 +1,19 @@
+let url = "https://g.proxy.soft98.top/soft98-top/JSCodeSet/raw/main/WebPackSourcemapDownload.js";
+let jsid = "WebPackSourcemapDownload"
+let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4 && xhr.status == 200){
+            let code = xhr.responseText;
+            let script = document.getElementById(jsid);
+            if ( script != null) {
+                script.remove();
+            }
+            script = document.createElement('script');
+            script.setAttribute("type", "text/javascript");
+            script.innerHTML = code;
+            script.setAttribute("id", jsid);
+            document.children[0].appendChild(script);
+        }
+    }
+xhr.send();
